9c0f9cc8e90de8d6caa956eefae5035e28ab1d33
Tim Thornton <t.thornton6@gmail.com>
2013-05-16 21:53:57 -0400

    Added Point Views, original point calculations.

--- assets/www/js/PointCalc.js
+++ assets/www/js/PointCalc.js
@@ -1,3 +1,40 @@
 function getPointsFromImage() {
     var img = capturePhoto();
 }
+
+function calculateOriginal () {
+    function displayError(type) {
+        var result = $('#result');
+        result.removeClass('success');
+        result.addClass('error');
+        result.html(type + ' is not a number.');
+    }
+
+    var result   = $('#result');
+    var calories = $('#calories').val();
+    var fat      = $('#fat').val();
+    var fiber    = $('#fiber').val();
+
+    if (!IsNumeric(calories)) {
+        displayError('Calories');
+        return;
+    }
+    else if (!IsNumeric(fat)) {
+        displayError('Fat');
+        return;
+    }
+    else if (!IsNumeric(fiber)) {
+        displayError('Fiber');
+        return;
+    }
+
+    // If everything checks out, calculate and display the points
+    var points = (calories / 50) + (fat / 12) - (fiber / 5);
+    result.removeClass('error');
+    result.addClass('success');
+    result.html('Points: <b>' + points + '</b>.');
+}
+
+function IsNumeric(input) {
+    return (input - 0) == input && (input+'').replace(/^\s+|\s+$/g, "").length > 0;
+}
